0b5d6bcb038374c13b9bafb294aaa451411cc7c9
shopoigraem <sashashevchuk1003@gmail.com>
2025-08-29 19:57:10 +0300

    fix qr

--- collectohub/coins/management/commands/fiq_qr.py
+++ collectohub/coins/management/commands/fiq_qr.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
         for coin in coins:
             print(coin.pk)
             # Правильний спосіб очищення ImageField
-            if coin.qr_code:
-                coin.qr_code.delete(save=False)  # Видаляє файл з диску
+            # if coin.qr_code:
+            #     coin.qr_code.delete(save=False)  # Видаляє файл з диску
             coin.qr_code = None  # Очищає поле в базі даних
             coin.save()
